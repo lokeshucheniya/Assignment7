66ea992bd85b3db95227cafa20ff6a193d87f97b
lokeshucheniya <lokeshucheniya15@gmail.com>
2018-08-04 18:18:02 +0530

    Update PaymentGateway.java

--- PaymentGateway.java
+++ PaymentGateway.java
@@ -1,19 +1,17 @@
-//Class that performs transfer of funds between two accounts 
+
 public class PaymentGateway {
-	//method to transfer the given amount from the src to target account.
+	//method to transfer the given amount from the source to target account.
 		public static boolean transfer(Account sender, Account reciever, double amount) 
 		{
-			//If the sender account balance = the given amount then the amount is
-			//transferred from the sender to the reciever account and true is returned
-			//otherwise false is returned.
+			
 			boolean status;
-			if(sender.getBalance() == amount) {
+			if(sender.getBalance() == amount) { 			//If account balance is same as given amount then perform the operations
 				sender.withdraw(amount);
 				reciever.deposit(amount);
 				status = true;
 			}
 			else {
-				status = false;
+				status = false;					
 			}
 			return status;
 		}
